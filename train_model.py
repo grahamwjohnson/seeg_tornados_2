c057efcc1a454aca1f648d1c60d386e7b2b115ef
grahamwjohnson <grahamwjohnson@gmail.com>
2025-01-01 13:04:42 -0600

    .

--- train_model.py
+++ train_model.py
@@ -53,6 +53,177 @@ def ddp_setup(gpu_id, world_size):
 
     init_process_group(backend="nccl", rank=gpu_id, world_size=world_size, timeout=datetime.timedelta(minutes=999999))
 
+
+def load_train_objs(
+    gpu_id, 
+    eon, 
+    pat_dir, 
+    train_val_pat_perc, 
+    intrapatient_dataset_style, 
+    train_hour_dataset_range, 
+    val_finetune_hour_dataset_range, 
+    val_unseen_hour_dataset_range, 
+    inference_selection, 
+
+    # BSE
+    core_weight_decay, 
+    head_weight_decay, 
+    
+    # transformer
+    max_seq_len, 
+    max_batch_size,
+    n_layers,
+    n_heads, 
+    multiple_of,
+    transformer_dim,
+    adamW_wd,
+    transformer_LR,
+    
+    **kwargs):
+
+    # Split pats into train and test
+    all_pats_dirs = glob.glob(f"{pat_dir}/*pat*")
+    all_pats_list = [x.split('/')[-1] for x in all_pats_dirs]
+
+    val_pats_count = int(np.ceil(train_val_pat_perc[1] * len(all_pats_list)))
+    val_pats_dirs = all_pats_dirs[-val_pats_count:]
+    val_pats_list = all_pats_list[-val_pats_count:]
+
+    train_pats_dirs = all_pats_dirs[:-val_pats_count]
+    train_pats_list = all_pats_list[:-val_pats_count]
+
+    # Sequential dataset used to run inference on train data and build PaCMAP projection
+    print(f"[GPU{str(gpu_id)}] Generating TRAIN dataset")
+    kwargs['dataset_pic_dir'] = kwargs['model_dir'] + '/dataset_bargraphs_BSE/Train_Grouped'
+    train_set = SEEG_Tornado_Dataset(
+        gpu_id=gpu_id, 
+        pat_list=train_pats_list,
+        pat_dirs=train_pats_dirs,
+        eon=eon, 
+        intrapatient_dataset_style=intrapatient_dataset_style, 
+        hour_dataset_range=train_hour_dataset_range,
+        single_pat_seq = False, 
+        **kwargs)
+    
+    print(f"[GPU{str(gpu_id)}] Generating VALIDATION FINETUNE dataset")
+    kwargs['dataset_pic_dir'] = kwargs['model_dir'] + '/dataset_bargraphs_BSE/Val_Grouped_Finetune'
+    val_finetune_set = SEEG_Tornado_Dataset(
+        gpu_id=gpu_id, 
+        eon=eon, 
+        pat_list=val_pats_list,
+        pat_dirs=val_pats_dirs,
+        intrapatient_dataset_style=intrapatient_dataset_style, 
+        hour_dataset_range=val_finetune_hour_dataset_range,
+        single_pat_seq = False,  
+        **kwargs)
+
+    print(f"[GPU{str(gpu_id)}] Generating VALIDATION UNSEEN dataset")
+    kwargs['dataset_pic_dir'] = kwargs['model_dir'] + '/dataset_bargraphs_BSE/Val_Grouped_Unseen'
+    val_unseen_set = SEEG_Tornado_Dataset(
+        gpu_id=gpu_id, 
+        eon=eon, 
+        pat_list=val_pats_list,
+        pat_dirs=val_pats_dirs,
+        intrapatient_dataset_style=intrapatient_dataset_style, 
+        hour_dataset_range=val_unseen_hour_dataset_range,
+        single_pat_seq = False,  
+        **kwargs)
+    
+    print(f"[GPU{str(gpu_id)}] Generating INDIVIDUAL TRAIN dataset")
+    kwargs['dataset_pic_dir'] = kwargs['model_dir'] + '/dataset_bargraphs_BSE/Train_Individual'
+    ind_train_datasets = [-1]*len(train_pats_list)
+    for pat_idx in range(0, len(train_pats_list)):
+        ind_train_datasets[pat_idx] = SEEG_Tornado_Dataset(
+            gpu_id=gpu_id, 
+            eon=eon, 
+            pat_list=[train_pats_list[pat_idx]],
+            pat_dirs=[train_pats_dirs[pat_idx]],
+            intrapatient_dataset_style=intrapatient_dataset_style, 
+            hour_dataset_range=train_hour_dataset_range,
+            single_pat_seq = True,  
+            **kwargs)
+
+    print(f"[GPU{str(gpu_id)}] Generating INDIVIDUAL VALIDATION FINETUNE dataset")
+    kwargs['dataset_pic_dir'] = kwargs['model_dir'] + '/dataset_bargraphs_BSE/Val_Individual_Finetune'
+    ind_val_finetune_datasets = [-1]*len(val_pats_list)
+    for pat_idx in range(0, len(val_pats_list)):
+        ind_val_finetune_datasets[pat_idx] = SEEG_Tornado_Dataset(
+            gpu_id=gpu_id, 
+            eon=eon, 
+            pat_list=[val_pats_list[pat_idx]],
+            pat_dirs=[val_pats_dirs[pat_idx]],
+            intrapatient_dataset_style=intrapatient_dataset_style, 
+            hour_dataset_range=val_finetune_hour_dataset_range,
+            single_pat_seq = True,  
+            **kwargs)
+
+    print(f"[GPU{str(gpu_id)}] Generating INDIVIDUAL VALIDATION UNSEEN dataset")
+    kwargs['dataset_pic_dir'] = kwargs['model_dir'] + '/dataset_bargraphs_BSE/Val_Individual_Unseen'
+    ind_val_unseen_datasets = [-1]*len(val_pats_list)
+    for pat_idx in range(0, len(val_pats_list)):
+        ind_val_unseen_datasets[pat_idx] = SEEG_Tornado_Dataset(
+            gpu_id=gpu_id, 
+            eon=eon, 
+            pat_list=[val_pats_list[pat_idx]],
+            pat_dirs=[val_pats_dirs[pat_idx]],
+            intrapatient_dataset_style=intrapatient_dataset_style, 
+            hour_dataset_range=val_unseen_hour_dataset_range,
+            single_pat_seq = True,  
+            **kwargs)
+    
+    # Build the model with swappable heads
+    train_enc_heads = [-1]*len(train_pats_list)
+    train_dec_heads = [-1]*len(train_pats_list)
+    train_hint_preppers = [-1]*len(train_pats_list)
+    for i in range(0, len(train_pats_list)):
+        train_enc_heads[i] = BSE_Enc_Head(pat_id=train_pats_list[i], num_channels=train_set.pat_num_channels[i], **kwargs).to(gpu_id)
+        train_dec_heads[i] = BSE_Dec_Head(pat_id=train_pats_list[i], num_channels=train_set.pat_num_channels[i], **kwargs).to(gpu_id)
+        train_hint_preppers[i] = BSE_Dec_Hint_Prep(pat_id=train_pats_list[i], num_channels=train_set.pat_num_channels[i], **kwargs).to(gpu_id)
+    train_heads = (train_enc_heads, train_dec_heads, train_hint_preppers)
+
+    val_enc_heads = [-1]*len(val_pats_list)
+    val_dec_heads = [-1]*len(val_pats_list)
+    val_hint_preppers = [-1]*len(val_pats_list)
+    for i in range(0, len(val_pats_list)):
+        val_enc_heads[i] = BSE_Enc_Head(pat_id=val_pats_list[i], num_channels=val_finetune_set.pat_num_channels[i], **kwargs).to(gpu_id)
+        val_dec_heads[i] = BSE_Dec_Head(pat_id=val_pats_list[i], num_channels=val_finetune_set.pat_num_channels[i], **kwargs).to(gpu_id)
+        val_hint_preppers[i] = BSE_Dec_Hint_Prep(pat_id=val_pats_list[i], num_channels=val_finetune_set.pat_num_channels[i], **kwargs).to(gpu_id)
+    val_heads = (val_enc_heads, val_dec_heads, val_hint_preppers)
+
+    # Build the core model
+    vae_core = BSE_Middle_VAE(gpu_id=gpu_id, **kwargs) 
+    vae_core = vae_core.to(gpu_id) # move to GPU here to avoid opt_core problems when loading states
+
+    # Build the optimizers, one for core and individual opts for swappable heads
+    opt_core = torch.optim.AdamW(vae_core.parameters(), lr=kwargs['LR_min_core'], weight_decay=core_weight_decay)
+    opts_train = Head_Optimizers(heads=train_heads, wd=head_weight_decay, lr=kwargs['LR_min_heads'])
+    opts_val = Head_Optimizers(heads=val_heads, wd=head_weight_decay, lr=kwargs['LR_min_heads'])
+    
+
+    ### Transformer ###
+    
+    # Make the transformer based on Llama3
+    model_args: ModelArgs = ModelArgs(
+        dim=transformer_dim,
+        vae_dim=kwargs['latent_dim'],
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+        n_layers=n_layers,
+        n_heads=n_heads,
+        multiple_of=multiple_of,
+        device=gpu_id
+    )
+    transformer = Transformer(model_args)
+    transformer = transformer.to(gpu_id)
+    # opt = torch.optim.Adam(transformer.parameters(), lr=LR_min)
+    transformer_opt = torch.optim.AdamW(transformer.parameters(), lr=transformer_LR, weight_decay=adamW_wd)
+    print(f"[GPU{gpu_id}] transformer loaded")
+
+    return train_set, val_finetune_set, val_unseen_set, ind_train_datasets, ind_val_finetune_datasets, ind_val_unseen_datasets, transformer, transformer_opt, vae_core, train_heads, val_heads, opt_core, opts_train, opts_val  #infer_set
+
+
+
+
 def main(         
     # Ordered variables
     gpu_id: int, 
@@ -88,21 +259,8 @@ def main(
     ddp_setup(gpu_id, world_size)
 
     print(f"[GPU{str(gpu_id)}] Loading training objects (datasets, model, opt_core)")
-    train_dataset, val_finetune_dataset, val_unseen_dataset, ind_train_datasets, ind_val_finetune_datasets, ind_val_unseen_datasets, lbm, lbm_opt, vae_core, train_heads, val_heads, opt_core, opts_train, opts_val  = load_train_objs(gpu_id=gpu_id, eon=eon, **kwargs) 
+    train_dataset, val_finetune_dataset, val_unseen_dataset, ind_train_datasets, ind_val_finetune_datasets, ind_val_unseen_datasets, transformer, transformer_opt, vae_core, train_heads, val_heads, opt_core, opts_train, opts_val  = load_train_objs(gpu_id=gpu_id, eon=eon, **kwargs) 
     
-    # Print VAE CORE model info (only do once on gpu_id==0)
-    if gpu_id == 0: 
-        utils_functions.print_model_summary(model=vae_core)
-
-        # Print the estimated LBM model size
-        utils_functions.print_model_summary(model=lbm)
-
-        # Save some code
-        save_code_path = kwargs['model_dir'] + '/code_archived'
-        if not os.path.exists(save_code_path): os.makedirs(save_code_path)
-        shutil.copyfile(vae_core.get_script_filename(), save_code_path + '/model_code.py')
-        shutil.copyfile(train_dataset.get_script_filename(), save_code_path + '/data_code.py')
-        shutil.copyfile(__file__, save_code_path + '/train_code.py')
 
     # Build dataloaders from datasets
     seq_workers = kwargs['num_dataloader_workers_SEQUENTIAL']
@@ -181,8 +339,8 @@ def main(
     trainer = Trainer(
         world_size=world_size,
         gpu_id=gpu_id, 
-        lbm=lbm,
-        lbm_opt=lbm_opt,
+        transformer=transformer,
+        transformer_opt=transformer_opt,
         vae_core=vae_core, 
         train_heads=train_heads,
         val_heads=val_heads,
